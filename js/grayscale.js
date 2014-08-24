9ef11fda078b66e84dee8f3c216cfcbc38b441ff
Liban Mohamed <libanmohamed@libans-air.home>
2014-08-23 21:26:25 -0400

    changed long and lat

--- js/grayscale.js
+++ js/grayscale.js
@@ -41,7 +41,7 @@ function init() {
         zoom: 15,
 
         // The latitude and longitude to center the map (always required)
-        center: new google.maps.LatLng(40.6700, -73.9400), // New York
+        center: new google.maps.LatLng(38.987060, -76.942559), // New York
 
         // Disables the default Google Maps UI components
         disableDefaultUI: true,
@@ -169,7 +169,7 @@ function init() {
 
     // Custom Map Marker Icon - Customize the map-marker.png file to customize your icon
     var image = 'img/map-marker.png';
-    var myLatLng = new google.maps.LatLng(40.6700, -73.9400);
+    var myLatLng = new google.maps.LatLng(38.987060, -76.942559);
     var beachMarker = new google.maps.Marker({
         position: myLatLng,
         map: map,
