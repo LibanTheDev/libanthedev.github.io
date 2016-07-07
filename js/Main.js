425226ac4f0b6fafacf58105e42f67c3bc750bef
LibanTheDev <liban.moh99@gmail.com>
2016-07-07 01:53:19 -0400

    redesign

--- js/Main.js
+++
@@ -1,4 +0,0 @@
-$('#lastFmWidget').lastfmNowPlaying({
-	apiKey: 'e008530e77039cf6d99a20c92621c4fb',
-	members: ['libanthedev']
-});
---
+++ main.js
@@ -0,0 +1,20 @@
+
+
+var $quoteMachine = $('.quote-machine');
+var html="";
+
+
+
+
+
+$.getJSON('https://wisdomapi.herokuapp.com/v1/random?callback=?', function(data){
+	console.log(data);
+	html+='<h2>Inspirational Quotes for Entrepreneurs</h2> <h1>'+data.content+'</h1> <p> -'+data.author.name+', '+data.author.company+'</p>';
+	$quoteMachine.html(html);
+
+	console.log(data);
+
+
+});
+
+ 
